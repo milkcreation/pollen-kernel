e39c2099288ab566ed9f18c085bb39d438059199
5upp0r7 <5upp0r7@23fad6b8-6d8e-48a5-9e8d-e4f1c0242fc1>
2021-06-18 19:35:54 +0000

    kernel >> 1.0.x-dev

--- kernel/branches/1.0/src/KernelInterface.php
+++ kernel/branches/1.0/src/KernelInterface.php
@@ -4,6 +4,9 @@ declare(strict_types=1);
 
 namespace Pollen\Kernel;
 
+use Pollen\Http\RequestInterface;
+use Pollen\Http\ResponseInterface;
+
 interface KernelInterface
 {
     /**
@@ -12,4 +15,32 @@ interface KernelInterface
      * @return ApplicationInterface
      */
     public function getApp(): ApplicationInterface;
+
+    /**
+     * Traitement de la requête HTTP.
+     *
+     * @param RequestInterface $request
+     *
+     * @return ResponseInterface
+     */
+    public function handle(RequestInterface $request): ResponseInterface;
+
+    /**
+     * Envoi de la réponse HTTP.
+     *
+     * @param ResponseInterface $response
+     *
+     * @return bool
+     */
+    public function send(ResponseInterface $response): bool;
+
+    /**
+     * Termine le cycle de la requête et de la réponse HTTP.
+     *
+     * @param RequestInterface $request
+     * @param ResponseInterface $response
+     *
+     * @return void
+     */
+    public function terminate(RequestInterface $request, ResponseInterface $response): void;
 }
